505197ca02114b8bf17225d93f1e64d248557d83
Lorenzo Andreotti <lorenzo.andreotti.2000@gmail.com>
2026-01-05 18:30:54 +0100

    Implements navigation between auth fragments
    Enables navigation between the welcome, login, and register fragments.
    This allows users to easily move between different stages of the
    authentication process.
    
    Also adds .idea files to .gitignore

--- app/src/main/java/it/mybooks/mybooks/RegisterFragment.java
+++ app/src/main/java/it/mybooks/mybooks/RegisterFragment.java
@@ -7,10 +7,12 @@ import android.os.Bundle;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.navigation.Navigation;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 public class RegisterFragment extends Fragment {
 
@@ -27,10 +29,14 @@ public class RegisterFragment extends Fragment {
     }
 
     @Override
-    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
-        super.onActivityCreated(savedInstanceState);
-        mViewModel = new ViewModelProvider(this).get(RegisterViewModel.class);
-        // TODO: Use the ViewModel
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        Button b = view.findViewById(R.id.buttonRegisterToLogin);
+
+        b.setOnClickListener(v -> {
+            Navigation.findNavController(v).navigate(R.id.action_registerFragment_to_loginFragment);
+        });
     }
 
 }
--- app/src/main/java/it/mybooks/mybooks/WelcomeFragment.java
+++ app/src/main/java/it/mybooks/mybooks/WelcomeFragment.java
@@ -7,10 +7,12 @@ import android.os.Bundle;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.navigation.Navigation;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 public class WelcomeFragment extends Fragment {
 
@@ -27,10 +29,13 @@ public class WelcomeFragment extends Fragment {
     }
 
     @Override
-    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
-        super.onActivityCreated(savedInstanceState);
-        mViewModel = new ViewModelProvider(this).get(WelcomeViewModel.class);
-        // TODO: Use the ViewModel
-    }
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
 
+        Button b = view.findViewById(R.id.buttonEmailAndPassword);
+
+        b.setOnClickListener(v -> {
+            Navigation.findNavController(v).navigate(R.id.action_welcomeFragment_to_loginFragment);
+        });
+    }
 }

faae0f59ba4480ede98629cf2d5133474a9155c2
Lorenzo Andreotti <l.andreotti5@campus.unimib.it>
2026-01-26 17:26:57 +0100

    Refactor toolbar setup and navigation handling in MainActivity

--- app/src/main/java/it/mybooks/mybooks/ui/MainActivity.java
+++ app/src/main/java/it/mybooks/mybooks/ui/MainActivity.java
@@ -9,12 +9,14 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 import androidx.navigation.NavController;
 import androidx.navigation.fragment.NavHostFragment;
+import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
 
 import it.mybooks.mybooks.R;
 
 public class MainActivity extends AppCompatActivity {
     private NavController navController;
+    private AppBarConfiguration appBarConfiguration;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,20 +29,29 @@ public class MainActivity extends AppCompatActivity {
             return insets;
         });
 
+        setSupportActionBar(findViewById(R.id.toolbar));
+
         NavHostFragment navHostFragment = (NavHostFragment) getSupportFragmentManager()
-                .findFragmentById(R.id.nav_graph);
+                .findFragmentById(R.id.fragmentContainerView);
 
         // 2. Ottieni il NavController
         if (navHostFragment != null) {
             navController = navHostFragment.getNavController();
 
+            appBarConfiguration = new AppBarConfiguration.Builder(
+                    R.id.welcomeFragment,
+                    R.id.profileFragment,
+                    R.id.searchFragment
+            ).build();
+
             // 3. Collega la ActionBar al NavController
-            NavigationUI.setupActionBarWithNavController(this, navController);
+            NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
         }
     }
 
     @Override
     public boolean onSupportNavigateUp() {
-        return navController.navigateUp() || super.onSupportNavigateUp();
+        return NavigationUI.navigateUp(navController, appBarConfiguration)
+                || super.onSupportNavigateUp();
     }
 }
